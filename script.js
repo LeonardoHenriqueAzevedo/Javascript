fb668163de3b8d9cf9453de81afd140dba925e50
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-03-09 11:03:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,19 +9,20 @@ const fetchPokemon = () => {
 
     Promise.all(pokemonPromises).then(pokemons => {
         const liPokemon = pokemons.reduce((accumulator, pokemon) => {
-            const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+            const types = pokemon.types.map(typeInfo => typeInfo.type.name);
 
             accumulator += `
             <li class="card">
-            <img class="card-image" ${types[0]}" alt="${pokemon.name} 
-            src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+            <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
               <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
               <p class="card-subtitle">${types.join(" | ")}</p>
             </li>`;
             return accumulator;
         }, "")
 
-        console.log(liPokemon)
+        const ul = document.querySelector('[data-js="pokedex"]');
+
+        ul.innerHTML = liPokemon;
     });
 };
 
