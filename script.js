46c73c99884dd103a83262550e864d8d28d03076
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-03-09 10:37:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const fetchPokemon = () => {
     const url = `https://pokeapi.co/api/v2/pokemon/`
-    fetch(url)
+    fetch(url).then(response => response.json()).then(pokemon => {
+        console.log(pokemon)
+    })
 }
 
 fetchPokemon();
